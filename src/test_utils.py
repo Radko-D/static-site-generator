cf2ff157f6df97bd4c328fb8ead5e24966a3e8c8
Radko <radoslav.d@icloud.com>
2024-07-08 18:22:05 +0300

    Method to split text on images an links

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode
-from utils import extract_markdown_images, extract_markdown_links, split_nodes_delimiter
+from utils import extract_markdown_images, extract_markdown_links, split_nodes_delimiter, split_nodes_image
 
 
 class TestUtils(unittest.TestCase):
@@ -26,6 +26,47 @@ class TestUtils(unittest.TestCase):
     def test_extract_markdown_links(self):
         text = "[Google](https://www.google.com)"
         self.assertEqual(extract_markdown_links(text), [("Google", "https://www.google.com")])
+    
+    def test_extract_markdown_links_no_links(self):
+        text = "This is a paragraph of text."
+        self.assertEqual(extract_markdown_links(text), [])
         
+    def test_extract_markdown_images_no_images(self):
+        text = "This is a paragraph of text."
+        self.assertEqual(extract_markdown_images(text), [])
+        
+    def test_extract_markdown_images_many_images(self):
+        text = "![image](https://www.google.com) ![image](https://www.google.com)"
+        self.assertEqual(extract_markdown_images(text), [("image", "https://www.google.com"), ("image", "https://www.google.com")])
+    
+    def test_extract_markdown_links_many_links(self):
+        text = "[Google](https://www.google.com) [Google](https://www.google.com)"
+        self.assertEqual(extract_markdown_links(text), [("Google", "https://www.google.com"), ("Google", "https://www.google.com")])
+    
+    def test_split_nodes_image(self):
+        node = TextNode("This is an image ![image](https://www.google.com) and this is follow-up text", "text")
+        new_nodes = split_nodes_image([node])
+        print(new_nodes)
+        self.assertEqual(new_nodes, [
+        TextNode("This is an image ", "text"),
+        TextNode("image", "image", "https://www.google.com"),
+        TextNode(" and this is follow-up text", "text")
+        ])
+    
+    def test_split_nodes_image_no_images(self):
+        node = TextNode("This is a paragraph of text.", "text")
+        new_nodes = split_nodes_image([node])
+        self.assertEqual(new_nodes, [node])
+
+    def test_split_nodes_image_many_images(self):
+        node = TextNode("This is an image ![image](https://www.google.com) and this is another image ![image](https://www.google.com)", "text")
+        new_nodes = split_nodes_image([node])
+        self.assertEqual(new_nodes, [
+        TextNode("This is an image ", "text"),
+        TextNode("image", "image", "https://www.google.com"),
+        TextNode(" and this is another image ", "text"),
+        TextNode("image", "image", "https://www.google1.com")
+        ])    
+    
 if __name__ == "__main__":
     unittest.main()
--- src/utils.py
+++ src/utils.py
@@ -10,15 +10,48 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             new_nodes.append(old_node)
             continue
         parts = old_node.text.split(delimiter)
-        if len(parts)%2 == 0:
+        if len(parts) % 2 == 0:
             raise Exception(f"Delimiter {delimiter} not closed in {old_node.text}")
         for i, part in enumerate(parts):
-            if i %2 == 0:
+            if i % 2 == 0:
                 new_nodes.append(TextNode(part, 'text'))
             else:
                 new_nodes.append(TextNode(part, text_type))
     return new_nodes
-    
+
+
+#TODO fix for multiple images
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        images = extract_markdown_images(old_node.text)
+        if old_node.text_type != "text" or images == []:
+            new_nodes.append(old_node)
+            continue
+        for image_tup in images:
+            parts = old_node.text.split(f"![{image_tup[0]}]({image_tup[1]})")
+            for i, part in enumerate(parts):
+                new_nodes.append(TextNode(part, 'text'))
+                if i == 0:
+                    new_nodes.append(TextNode(image_tup[0], 'image', image_tup[1]))
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        images = extract_markdown_links(old_node.text)
+        if old_node.text_type != "text" or images == []:
+            new_nodes.append(old_node)
+            continue
+        for link_tup in images:
+            parts = old_node.text.split(f"![{link_tup[0]}]({link_tup[1]})")
+            for i, part in enumerate(parts):
+                if i % 2 == 0:
+                    new_nodes.append(TextNode(part, 'text'))
+                    if i == 0:
+                        new_nodes.append(TextNode(link_tup[0], 'link', link_tup[1]))
+    return new_nodes        
+
 def extract_markdown_images(text): 
     return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
 
